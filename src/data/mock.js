312ebc3fe52f4f62a6ec9eae16e6becc5a2107f7
kritin7 <62542754+kritin7@users.noreply.github.com>
2026-03-11 03:26:47 +0530

    Update mock.js

--- src/data/mock.js
+++ src/data/mock.js
@@ -91,5 +91,14 @@ export const teamMembers = [
     institutions: ['Piramal', 'IITB'],
     image: '/kritin.jpeg',
     linkedin: 'https://www.linkedin.com/in/kritin-agarwal-342306156/'
+  },
+  {
+    id: 2,
+    name: 'Yuvraj Yadav',
+    role: 'CTO',
+    bio: 'Senior Data Scientist at Piramal Finance, IITD.',
+    institutions: ['Piramal', 'IITD'],
+    image: '/yuvraj.jpeg',
+    linkedin: 'https://www.linkedin.com/in/yuvraj-yadav-256b891b4/'
   }
 ];
